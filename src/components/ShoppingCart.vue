46639524a4543c02b95a8fe353b6cef96da17661
Abraham Ortega <aortega@lean-tech.io>
2022-02-13 00:28:52 -0500

    changes

--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -1,23 +1,33 @@
 <template>
-    <div>
-        <div class="row">
-            <div class="col">
-              <q-btn round icon="shopping_cart cursor-pointer" flat text-color="white"  v-on:click="onCLickToggleDrawer">
-                 <q-badge floating color="red" rounded v-if="count">
-                    {{count}}
-                </q-badge>
-              </q-btn>
-            </div>
-            <div class="col">
-               Total: {{
-                    (total).toLocaleString("en-US", {
-                      style: "currency",
-                      currency: "USD",
-                    })
-                }}
-            </div>
-        </div>
+  <div>
+    <div class="row text-center">
+      <div class="col">
+        <q-btn
+          class="cursor-pointer"
+          round
+          icon="shopping_cart"
+          flat
+          text-color="white"
+          v-on:click="onCLickToggleDrawer"
+        >
+          <q-badge floating color="red" rounded v-if="count">
+            {{ count }}
+          </q-badge>
+        </q-btn>
+      </div>
+      <div class="col aling-center">
+        <span>
+          Total:
+          {{
+            total.toLocaleString("en-US", {
+              style: "currency",
+              currency: "USD",
+            })
+          }}
+        </span>
+      </div>
     </div>
+</div>
 </template>
 <script lang="ts">
 import { defineComponent ,computed} from 'vue';
